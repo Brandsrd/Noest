ad54a4807ac143c4850b32185ee764f995e73b4e
Ruben Brands <rdbrands@gmail.com>
2016-01-21 15:27:27 +0100

    added static pages controller

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,21 @@
+# van Valureux project
+
+# class LandingController < ApplicationController
+#   skip_before_filter :authenticate_user!
+
+#   def landing
+#     render layout: false
+#   end
+
+#   # def landing2
+#   #   if current_user
+#   #     redirect_to '/home/index'
+#   #   else
+#   #     render layout: false
+#   #   end
+#   # end
+
+#   # def signup
+#   #   render layout: false
+#   # end
+# end
